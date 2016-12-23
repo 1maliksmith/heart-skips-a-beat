7d351c7ffbf1f269a1c68c56d2d617990da10903
Malik Smith <maliksmith@Malik-Smiths-MacBook-Pro.local>
2016-12-22 23:26:07 -0500

    Added HSAB player embed

--- index.php
+++ index.php
@@ -15,7 +15,6 @@
 	</head>
 	<body>
 		<div class="container">
-		<h1>Hello World!</h1>
-		</div>
+<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/299247830%3Fsecret_token%3Ds-nJMv4&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>		</div>
 	</body>
 </html>
